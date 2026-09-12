2884c12ec44b83599d4ceaa2099f8297a75eb959
Ahnaf <mdahnaf3696@gmail.com>
2026-09-12 10:42:19 +0530

    Dev 2 changed common line

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("greet").innerText = "Hello from Our Team!";
 document.getElementById("timeMsg").innerText = "Good Morning, Team!";
-console.log("Team Project");
+console.log("Team Project - Dev 2");
