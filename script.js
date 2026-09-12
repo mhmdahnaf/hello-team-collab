4d22834040ba25d9fb512130e599769787a4198e
Ahnaf <mdahnaf3696@gmail.com>
2026-09-12 10:27:27 +0530

    Updated time message

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 document.getElementById("greet").innerText = "Hello from Our Team!";
-document.getElementById("timeMsg").innerText = "Hello from Team Updated!";
+document.getElementById("timeMsg").innerText = "Good Morning, Team!";
